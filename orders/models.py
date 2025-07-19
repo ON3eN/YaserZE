718346a71898ccfdc72ca8d149b71b7cb9e82bd6
ON3eN <goto6946@gmail.com>
2025-07-19 21:32:49 +0300

    اضافة الموديلز

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    customer_name = models.CharField(max_length=100)
+    order_date = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"طلب {self.id} - {self.customer_name}"
