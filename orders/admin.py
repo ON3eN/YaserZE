ea062c81788f775e161c3446b2fc751a5f07b454
ON3eN <goto6946@gmail.com>
2025-07-19 21:36:53 +0300

    ادمنز

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer_name', 'order_date', 'is_paid']
+    search_fields = ['customer_name']
+    list_filter = ['is_paid', 'order_date']
